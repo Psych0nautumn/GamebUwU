f23a93816fed97f74cb4dee9d033ebba0f97bc9d
Psych0nautumn <autumnpaige159@gmail.com>
2022-04-18 00:03:24 -0400

    added basic disassembler

--- untitled/src/main/java/Loader.java
+++ untitled/src/main/java/Loader.java
@@ -20,12 +20,21 @@ public class Loader {
         }
 
 
+
         try (BufferedInputStream romStream = new BufferedInputStream(inputStream)) {
             int i;
             int count = 0;
             while ((i = romStream.read()) != -1) {
-                System.out.printf("%02x ", i);
-                System.out.println("hi");
+
+                HashMap<String, HashMap<String, Opcodes.instruction>> allInstructions;
+                allInstructions = Opcodes.getInstructions();
+//                System.out.printf("%02x ", i);
+                String hex = String.format("%02x", i);
+
+                String opcode = "0x" + hex.toUpperCase();
+                System.out.println(opcode);
+
+                System.out.println("pre: " + allInstructions.get("prefixed").get(opcode).mnemonic + " " + "unpre: " + allInstructions.get("unprefixed").get(opcode).mnemonic );
                 count++;
 
                 if (count == 16) {
